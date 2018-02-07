f31ebb632e2d92b481f9f1a2c6557ff510981809
jocekrlnn <jockerlnn@rambler.ru>
2018-02-07 17:17:00 +0300

    123

---
+++ TestGit/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Class1
+    {
+        private string m_name = "";
+
+        public string Name { get => m_name; set => m_name = value; }
+    }
+}
